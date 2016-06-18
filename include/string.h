eb76cd60c7f795e493beab5cdcc4b50eee315e8e
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-06-18 19:44:29 +0200

    bzero() implemented in assembly

--- include/string.h
+++ include/string.h
@@ -8,7 +8,7 @@ char *strncpy(char *dest, const char *src, size_t n);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 
-void bzero(void *s, size_t n);
+void bzero(void *s, size_t n) __attribute__((weak));
 void *memset(void *s, uint8_t c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);

a98c7f162d78b471b5a7caa8002f46554cf650c5
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-06-19 10:01:51 +0200

    pool of block buffers implemented

---
+++ fs/blkpool.c
@@ -0,0 +1,82 @@
+#include <blkpool.h>
+
+#include <hdd.h>
+
+#include <string.h>
+
+#define POOL_SIZE   8
+static bufblk_t _[POOL_SIZE];
+
+static inline
+void _init(bufblk_t *bufblk)
+{
+    bzero(bufblk, sizeof(*bufblk));
+}
+
+void blkpool_init()
+{
+    for (int i = 0; i < POOL_SIZE; i++)
+        _init(_ + i);
+}
+
+static int _readblk(blk_num_t blk_num, block_t *blk)
+{
+    if (hdd_readblk(blk_num, blk))
+        return 1;
+
+    return 0;
+}
+
+static bufblk_t *_get_free()
+{
+    for (int i = 0; i < POOL_SIZE; i++)
+        if (!_[i].locked) {
+            _[i].locked = true;
+            return _ + i;
+        }
+
+    return NULL; // no free bufs
+}
+
+
+bufblk_t *blkpool_getblk(blk_num_t blk_num)
+{
+    for (int i = 0; i < POOL_SIZE; i++)
+        if (_[i].valid && blk_num == _[i].blk_num) {
+            // TODO if locked fall asleep and retry
+            
+            _[i].locked = true;
+            return _ + i; 
+        }
+
+    // block not found in the buffer pool
+
+    bufblk_t *bufblk;
+
+    if (!(bufblk = _get_free()))
+        return NULL;
+
+    if (_readblk(blk_num, &bufblk->block)) {
+        blkpool_putblk(bufblk);
+        return NULL;
+    }
+
+    return bufblk;
+}
+
+
+void blkpool_putblk(bufblk_t *bufblk)
+{
+    bufblk->locked = false;
+}
+
+unsigned blkpool_get_num_free()
+{
+    unsigned count;
+
+    for (int i = 0; i < POOL_SIZE; i++)
+        if (!_[i].locked)
+            count++;
+
+    return count;
+}
---
+++ fs/blkpool.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <block.h>
+
+#include <stdbool.h>
+
+typedef struct {
+    bool        valid;
+    bool        locked;
+    blk_num_t   blk_num;
+    block_t     block; 
+} bufblk_t;
+
+
+void blkpool_init();
+bufblk_t *blkpool_getblk(blk_num_t);
+void blkpool_putblk(bufblk_t *);
+unsigned blkpool_get_num_free();

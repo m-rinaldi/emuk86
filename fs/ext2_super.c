ec359fa2d35d39e404f1b9ffae842a6b04e245f2
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-06-19 10:08:29 +0200

    ext2's superblock implementation code

---
+++ fs/ext2_super.c
@@ -0,0 +1,228 @@
+#include <ext2_super.h>
+
+#include <blkpool.h>
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+/*
+    what to do with the buffer cache?
+    should this module keep the superblock always "locked" and only let him
+    modify the superblock?
+
+    I would like to keep it uniformly and access the superblock through the
+    buffer cache
+ */
+
+static ext2_super_t _;
+static unsigned _num_groups;
+static const char *_strerr = "unknown";
+
+#define STR_BUF_LEN     63
+static char _strbuf[STR_BUF_LEN+1];
+
+#define SUPERBLOCK_BLK_NUM  1
+static int _load()
+{
+    bufblk_t *bufblk;
+
+    if (!(bufblk = blkpool_getblk(SUPERBLOCK_BLK_NUM)))
+        return 1;
+
+    memcpy(&_, &bufblk->block, sizeof(_)); 
+
+    // TODO keep the buffer block corresponding to the superblock locked?
+    blkpool_putblk(bufblk);
+    
+    return 0;
+}
+
+static inline
+bool _is_revision_zero()
+{
+    if (EXT2_GOOD_OLD_REV == _.rev_level)
+        return true;
+
+    _strerr = "revision level is not zero";
+    return false;        
+}
+
+static inline
+bool _is_block_size_supported()
+{
+    // only a block size of 1024 bytes is supported
+
+    // block size = 1024 << log_block_size
+    // block size == 1024 <--> log_block == 0
+    if (_.log_block_size) {
+        // block size is usually be 1k, 2k, 4k or 8k
+        // however, some systems allow ever larger blocks
+        // the error message is limited to 32k block size (more than enough)
+        // TODO replace with snprintf
+        sprintf(_strbuf, "block size is %d and not 1024 bytes",
+                         0xffff & (1024 << _.log_block_size));
+        _strerr = _strbuf;
+        return false;
+    }
+
+    return true;
+}
+
+static inline
+bool _is_inode_size_supported()
+{
+    // TODO check against sizeof(ext2_inode_t) instead of the magic number 128
+    if (_.inode_size != 128) {
+        // TODO message
+        _strerr = "inode size not supported";
+        return false;
+    }
+        
+
+    return true;
+}
+
+static inline
+bool _has_incompatible_features()
+{
+    if (_.feature_incompat) {
+        _strerr = "file system requires some unsupported features";
+        return true;
+    }
+    
+    return false;
+}
+
+static bool _is_supported()
+{
+/*
+    if (!_is_revision_zero()) 
+        return false;
+*/
+
+    if (!_is_block_size_supported())
+        return false;
+
+    if (!_is_inode_size_supported())
+        return false;
+
+    if (_has_incompatible_features()) 
+        return false;
+
+
+    return true;
+}
+
+static inline
+bool _has_errors()
+{
+    if (EXT2_VALID_FS == _.state)
+        return false; // file system is clean
+
+    // TODO if error check what kind of error
+    //      EXT2_ERRORS_CONTINUE
+    //      EXT2_ERRORS_RO
+    //      EXT2_ERRORS_PANIC
+
+    _strerr = "file system was not cleanly unmounted";
+    return true;
+}
+
+static int _calc_num_groups(unsigned *num_groups)
+{
+    if (!_.blocks_per_group || !_.inodes_per_group) {
+        _strerr = "zero blocks per group or inodes per group";
+        return 1;
+    }
+    
+    unsigned int num_blk_grps, num_ino_grps;
+
+    // round up integer division
+    num_blk_grps = (_.blocks_count - 1) / _.blocks_per_group + 1;
+    num_ino_grps = (_.inodes_count - 1) / _.inodes_per_group + 1;
+
+    if (num_blk_grps != num_ino_grps) {
+        _strerr = "inconsistent number of block groups";
+        return 1;
+    }
+
+    *num_groups = num_blk_grps; 
+
+    return 0;    
+}
+
+int ext2_super_init()
+{
+    if (_load())
+        return 1;
+
+    if (!_is_supported())
+        return 1;
+
+    if (_has_errors())
+        return 1;
+
+    if (_calc_num_groups(&_num_groups))
+        return 1;
+
+    return 0;
+}
+
+const char *ext2_super_strerr()
+{
+    return _strerr;
+}
+
+const ext2_super_t *ext2_super_get()
+{
+    return &_;
+}
+
+void ext2_super_display()
+{
+    printf("file system: %s, ", EXT2_SUPER_MAGIC == _.magic ? "ext2" : "???" );
+
+    {
+        const char *str;
+
+        switch (_.rev_level) {
+        case EXT2_GOOD_OLD_REV:
+            str = "0";
+            break;
+
+        case EXT2_DYNAMIC_REV:
+            str = "1";
+            break;
+
+        default:
+            str = "unkown";
+        }
+
+        printf("revision %s\n\n", str);
+    }
+    
+    printf("state: %d\n", _.state);
+    
+    printf("block size:   %8d\n", 1024 << _.log_block_size);
+
+    printf("\n");
+
+    printf("inodes count: %8d\n", _.inodes_count);
+    printf("blocks count: %8d\n", _.blocks_count);
+   
+    printf("\n");
+ 
+    printf("free inodes:  %8d\n", _.free_inodes_count);
+    printf("free blocks:  %8d\n", _.free_blocks_count);
+
+    printf("\n");
+
+    printf("blocks per group: %4d\n", _.blocks_per_group);
+    printf("inodes per group: %4d\n", _.inodes_per_group);
+
+    printf("\n");
+
+    printf("first inode: %d\n", _.first_ino);
+    printf("inode size: %4d\n", _.inode_size);
+}
---
+++ fs/ext2_super.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <stdint.h>
+
+/*
+ The following data structure for the ext2's superblock was created with the
+ help of the great work done by Dave Poirier on the ext2-doc project.
+ A copy his book, "The Second Extended File System", which I employed, is
+ available at http://www.nongnu.org/ext2-doc/ext2.html
+ */
+
+// magic
+#define EXT2_SUPER_MAGIC        0xef53
+
+// state
+#define EXT2_VALID_FS           1
+#define EXT2_ERROR_FS           2
+
+// errors
+#define EXT2_ERRORS_CONTINUE    1   // continue as if nothing happend
+#define EXT2_ERRORS_RO          2   // remount read-only
+#define EXT2_ERRORS_PANIC       3   // cause a kernel panic
+
+// creator os
+#define EXT2_OS_LINUX           0
+#define EXT2_OS_HURD            1
+#define EXT2_OS_MASIX           2
+#define EXT2_OS_FREEBSD         3
+#define EXT2_OS_LITES           4
+
+#define EXT2_GOOD_OLD_REV       0
+#define EXT2_DYNAMIC_REV        1
+
+// feature_incompat
+#define EXT2_FEATURE_INCOMPAT_COMPRESSION   0x0001  // compression is used
+// directory entries contain a type field
+#define EXT2_FEATURE_INCOMPAT_FILETYPE      0x0002
+// file system needs to replay its journal
+#define EXT2_FEATURE_INCOMPAT_RECOVER       0x0004
+// file system uses a journal device
+
+// rev_level
+#define EXT2_FEATURE_INCOMPAT_JOURNAL_DEV   0x0008
+#define EXT2_FEATURE_INCOMPAT_META_BG       0x0010
+
+
+typedef struct {
+    uint32_t    inodes_count;
+    uint32_t    blocks_count; 
+    uint32_t    r_blocks_count; // blocks reserved for the superuser
+    uint32_t    free_blocks_count;
+    uint32_t    free_inodes_count;
+    uint32_t    first_data_block;
+    uint32_t    log_block_size; // block size = 1024 << log_block_size
+
+    // log_frag_size >  0       --> fragment size = 1024 << log_frag_size
+    // log_frag_size <= 0       --> fragment size = 1024 >> -log_frag_size
+    uint32_t    log_frag_size;
+    uint32_t    blocks_per_group;
+    uint32_t    frags_per_group;
+    uint32_t    inodes_per_group;
+    uint32_t    mtime; // time of the last time the file system was mounted
+    uint32_t    wtime; // time of the last write access to the file system
+    uint16_t    mnt_count; // mounts since the last check
+    uint16_t    max_mnt_count; // mounts allowed before a check
+    uint16_t    magic; // identifier for the ext2 fs: 0xef53
+
+    // EXT2_VALID_FS    --> unmonted cleanly
+    // EXT2_ERROR_FS    --> errors detected
+    uint16_t    state;
+
+    // EXT2_ERRORS_CONTINUE --> continue as if nothing happened
+    // EXT2_ERRORS_RO       --> remount as read-only file system
+    // EXT2_ERRORS_PANIC    --> cause a kernel panic
+    uint16_t    errors;
+    uint16_t    minor_rev_level;
+    uint32_t    last_check;
+    uint32_t    check_interval;
+    uint32_t    creator_os;
+
+    // EXT2_GOOD_OLD_REV    --> revision 0
+    // EXT2_DYNAMIC_REV     --> revision 1: variable inode sizes, xattr ...
+    uint32_t    rev_level;    
+    uint16_t    def_resuid; // default user id for reserved blocks
+    uint16_t    def_resgid; // default group id for reserved blocks
+    
+    // EXT2_DYNAMIC_REV specific
+    uint32_t    first_ino; // first inode usable for standard files
+    uint16_t    inode_size; // in revision 0, this value is fixed to 128
+    uint16_t    block_group_nr; // block group number hosting this superblock
+    uint32_t    feature_compat;     // optional features present
+    
+    // =========================
+    // required features present
+    // =========================
+    // the file system implementation should refuse to mount the file system
+    // if any of the indicated feature is unsopported
+
+    // an implementation not supporting these features would be unable to
+    // properly use the file system 
+    uint32_t    feature_incompat;
+
+    // rest of EXT2_DYNAMIC_REV specific stuff
+    uint8_t     dynamic_rev[164];
+
+    uint8_t     unused[760];
+        
+} __attribute__((packed)) ext2_super_t;
+
+
+int ext2_super_init();
+const char *ext2_super_strerr();
+void ext2_super_display();
+const ext2_super_t *ext2_super_get();

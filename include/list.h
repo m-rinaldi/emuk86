915c39415bfcdc80a9b341a1af36e6d089393bd4
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-06-21 19:18:26 +0200

    list_get_head_node() and list_get_tail_node() implemented

--- include/list.h
+++ include/list.h
@@ -32,6 +32,12 @@ list_node_t *node_get_next(const list_node_t *node)
     return node->next;
 }
 
+static inline
+list_node_t *node_get_prev(const list_node_t *node)
+{
+    return node->prev;
+}
+
 // TODO node_insert_before()
 // TODO node_insert_after()
 /******************************************************************************/
@@ -56,7 +62,19 @@ void list_init(list_t *lst)
 static inline
 bool list_is_empty(const list_t * lst)
 {
-    return lst->head.prev == lst->head.next;
+    /**********************************
+                list empty
+                    ^
+                    |
+                    v
+    lst->head.prev == &lst->head
+                    ^
+                    |
+                    v
+    lst->head.next == &lst->head
+
+    ***********************************/
+    return lst->head.prev == &lst->head;
 }
 
 // is node the first node of the list?
@@ -87,19 +105,28 @@ void list_insert(list_t *lst, list_node_t *node)
     // node becomes the new tail
     node->prev = lst->head.prev;
     node->next = &lst->head;
+
+    // update the previous tail
     lst->head.prev->next = node;
+
+    // update the dummy node
     lst->head.prev = node; 
 }
 
 static inline
 list_node_t *list_get_head_node(const list_t *lst)
 {
+    if (list_is_empty(lst))
+        return NULL;
+
     return lst->head.next;
 }
 
 static inline
 list_node_t *list_get_tail_node(const list_t *lst)
 {
+    if (list_is_empty(lst))
+        return NULL;
+
     return lst->head.prev;
 }
-

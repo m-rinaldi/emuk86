a97d0b975c11d8dff4b3ccc54fbdec9f8abba189
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-07-24 11:44:58 +0200

    file table implemented

---
+++ fs/file_table.c
@@ -0,0 +1,76 @@
+#include <file_table.h>
+
+#include <ipool.h>
+#include <iterator.h>
+
+#define NUM_ENTRIES     8U
+
+static file_table_entry_t _[NUM_ENTRIES];
+static list_t _free_list;
+
+typedef void (* iterate_func_t)(file_table_entry_t *);
+
+DEFINE_ITERATE(_, NUM_ENTRIES, file_table_entry_t, iterate_func_t)
+
+static void _init(file_table_entry_t *fte)
+{
+    bzero(fte, sizeof(*fte));
+    
+    node_init(&fte->free_node);
+    // add file table entry to the list of free entries
+    list_insert(&_free_list, &fte->free_node);
+}
+
+void file_table_init()
+{
+    list_init(&_free_list);
+
+    _iterate(_init);
+}
+
+static file_table_entry_t *_alloc_entry()
+{
+    list_node_t *node;
+
+    if (!(node = list_get_head_node(&_free_list))) {
+        // TODO error msg "no free file talbe entries"
+        return NULL;
+    }
+
+    node_remove(node);
+    return node_get_container(node, file_table_entry_t, free_node);
+}
+
+file_table_entry_t *file_table_alloc_entry(uint8_t mode, inode_t *ino)
+{
+    file_table_entry_t *fte;
+
+    if (!(fte = _alloc_entry()))
+        return NULL;
+
+    // initialize just allocted file table entry
+    fte->count  = 0;
+    fte->offset = 0;
+    fte->mode   = mode;
+    fte->inode  = ino;
+
+    return fte;
+}
+
+file_table_entry_t *file_table_dup_entry(file_table_entry_t *fte)
+{
+    fte->count++;
+    return fte;
+}
+
+static void _dealloc_entry(file_table_entry_t *fte)
+{
+    ipool_puti(fte->inode);
+    list_insert(&_free_list, &fte->free_node);
+}
+
+void file_table_release(file_table_entry_t *fte)
+{
+    if (!--fte->count)
+        _dealloc_entry(fte);
+}
---
+++ include/fs/file_table.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <list.h>
+#include <inode.h>
+
+#include <stdbool.h>
+
+#define FTE_APPEND  0x1
+#define FTE_WRITE   0x2
+
+typedef struct {
+    unsigned    count;
+    uint8_t     mode; // append, read-only, write
+
+    // where the next read() or write() it is expected to begin
+    uint32_t offset;
+
+    // pointer to a inode on the inode table
+    inode_t *inode;
+
+    // node on a free list
+    list_node_t free_node;
+} file_table_entry_t;
+
+file_table_entry_t *file_table_alloc_entry(uint8_t mode, inode_t *);
+file_table_entry_t *file_table_dup_entry(file_table_entry_t *);
+void file_table_release(file_table_entry_t *fte);

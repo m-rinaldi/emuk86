28f4a079799ee8b1310ed625a1a70296eaba17de
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-09-12 20:02:33 +0200

    "blk_num" renamed to "num"

--- include/fs/blkpool.h
+++ include/fs/blkpool.h
@@ -1,18 +1,23 @@
 #pragma once
 
 #include <fs/block.h>
-
+#include <list.h>
 #include <stdbool.h>
 
 typedef struct {
     bool        valid;
     bool        locked;
-    blk_num_t   blk_num;
-    block_t     block; 
+
+    // TODO add dirty flag
+    blk_num_t   num;
+    block_t     block;
+
+    list_node_t free_node;
 } bufblk_t;
 
 
 void blkpool_init();
 bufblk_t *blkpool_getblk(blk_num_t);
+bufblk_t *blkpool_get_any();
 void blkpool_putblk(bufblk_t *);
 unsigned blkpool_get_num_free();
